43a27b1e79a5f02d686f7d0b4830cf499a574bb7
aibix <aibix@aibix.io>
2024-07-24 18:43:34 +0200

    cpu type geändert

--- create-vm-vyos.sh
+++ create-vm-vyos.sh
@@ -22,7 +22,7 @@ then
 	create)
 	    vmid=${node}0${provider}0`printf '%02d' $router`
 	    ## Create VM, import disk and define boot order
-	    qm create $vmid --name "p${provider}r${router}v" --ostype l26 --memory 2048 --balloon 2048 --cpu cputype=host --cores 2 --scsihw virtio-scsi-single --net0 virtio,bridge=ABXMGMTN,mtu=1500,macaddr="${mgmtmac}"
+	    qm create $vmid --name "p${provider}r${router}v" --ostype l26 --memory 2048 --balloon 2048 --cpu cputype=x86-64-v2-AES --cores 2 --scsihw virtio-scsi-single --net0 virtio,bridge=ABXMGMTN,mtu=1500,macaddr="${mgmtmac}"
 	    qm importdisk $vmid vyos-qcow/vyos-1.5.0-cloud-init-10G-qemu.qcow2 local-zfs
 	    qm set $vmid --virtio0 local-zfs:vm-$vmid-disk-0,iothread=1
 	    qm set $vmid --boot order=virtio0
